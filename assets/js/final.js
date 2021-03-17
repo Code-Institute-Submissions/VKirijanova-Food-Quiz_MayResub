5997d7c3b37e6c7fa7d4b85f9b448d6254c1ec55
VKirijanova <v.kirijanova@gmail.com>
2021-03-17 13:58:58 +0000

    created feedback  form with working javascript and style it

--- assets/js/final.js
+++ assets/js/final.js
@@ -6,3 +6,24 @@ finalScore.innerText = mostRecentScore;
 const score = {
     score: mostRecentScore,
 };
+
+/* Sending emails */
+function sendMail(contactForm) {
+  emailjs
+    .send("service_edl6rt5", "template_t45mpqs", {
+      from_name: contactForm.name.value,
+      from_email: contactForm.emailaddress.value,
+      project_request: contactForm.projectsummary.value,
+    })
+    .then(
+      function (response) {
+        console.log("SUCCESS", response);
+        window.alert("Thank you for your message, we will get back to you shortly");
+      },
+      function (error) {
+        console.log("FAILED", error);
+        window.alert("Oops! Something went wrong and we couldn't send your message!")
+      }
+    );
+  return false;
+}
